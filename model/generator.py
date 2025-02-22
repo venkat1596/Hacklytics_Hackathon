84bb377b330145a89944a56487e53ce269d38236
venkat <vbommireddy@fsu.edu>
2025-02-21 19:54:30 -0500

    Added a requirements.txt file.

--- model/generator.py
+++ model/generator.py
@@ -23,14 +23,12 @@ class DoubleConvBlock(nn.Module):
         self.smish= Smish()#nn.ReLU(inplace=True)
 
     def forward(self, x):
-        x_copy = x.clone()
         x = self.conv1(x)
         x = self.norm(x)
         x = self.smish(x)
         x = self.conv2(x)
         if self.use_act:
             x = self.smish(x)
-        x += x_copy
         return x
 
 
@@ -50,7 +48,7 @@ class Downsample(nn.Module):
     def __init__(self, features):
         super().__init__()
         self.pad = nn.ReflectionPad3d(1)
-        self.conv = nn.Conv3d(features, features, kernel_size=3, stride=2)
+        self.conv = nn.Conv3d(features, features, kernel_size=3, stride=(1, 2, 2))
 
     def forward(self, x):
         x = self.conv(self.pad(x))
@@ -72,44 +70,19 @@ class DilatedConvBlock(nn.Module):
         x += x_copy
         return x
 
-
-class UpConvBlock(nn.Module):
-    def __init__(self, in_features, up_scale):
-        super(UpConvBlock, self).__init__()
-        self.up_factor = 2
-        layers = self.make_deconv_layers(in_features, up_scale)
-        assert layers is not None, layers
-        self.features = nn.Sequential(*layers)
-
-    def make_deconv_layers(self, in_features, up_scale):
+class Upsampler(nn.Module):
+    def __init__(self, features, upscale, stride):
+        super().__init__()
         layers = []
-
-        for i in range(up_scale):
-            # Fixed kernel size of 2 for each upsampling step
-            kernel_size = 2
-            # Calculate appropriate padding
-            pad = kernel_size // 2
-            out_features = in_features
-
-            # 1x1x1 convolution to adjust feature channels
-            layers.append(nn.Conv3d(in_features, out_features, 1))
+        for _ in range(upscale):
+            layers.append(nn.Conv3d(features, features, 3, padding=1))
             layers.append(Smish())
-
-            # Transposed convolution for upsampling
-            layers.append(nn.ConvTranspose3d(
-                out_features, out_features,
-                kernel_size=kernel_size,
-                stride=2,
-                padding=pad,
-                output_padding=1  # Ensures correct output size
-            ))
-
-            in_features = out_features
-
-        return layers
+            layers.append(nn.ConvTranspose3d(features, features, kernel_size=3, padding=1, stride=stride))
+        self.conv_t = nn.Sequential(*layers)
 
     def forward(self, x):
-        return self.features(x)
+        x = self.conv_t(x)
+        return x
 
 class DoubleFusion(nn.Module):
     def __init__(self, in_ch):
@@ -134,30 +107,23 @@ class Unet(nn.Module):
 
         # Encoder
         # block 1
-        self.conv1 = SingleConv(in_channels, features)
+        self.conv1 = DoubleConvBlock(in_channels, features, stride=2)
 
         # block 2
-        self.conv2 = DoubleConvBlock(features, features * 2)
-        self.down2 = Downsample(features * 2)
+        self.conv2 = DilatedConvBlock(features, 3, dilation=2)
+        self.down2 = Downsample(features)
 
         # block 3
-        self.conv3 = DilatedConvBlock(features*2, 3, dilation=2)
-        self.down3 = Downsample(features*2)
-
-        # block 4
-        self.conv4 = DilatedConvBlock(features*2, 3, dilation=2)
+        self.conv3 = DilatedConvBlock(features, 3, dilation=2)
         # Decoder
 
         # up blocks
-        self.upblock3 = UpConvBlock(features*2, 1)
-        self.upblock2 = UpConvBlock(features*2, 1)
-        self.correctionblock2 = SingleConv(features*2, features)
+        self.upblock3 = Upsampler(features, 1, stride=(1, 2, 2))
+        self.upblock2 = Upsampler(features, 1, stride=(2, 2, 2))
 
         # decoder fuse block
-        self.fuse_block3 = DoubleFusion(features * 4)
-        self.fuse_conv3 = SingleConv(features * 4, features)
-        self.fuse_block2 = DoubleFusion(features * 2)
-        self.fuse_conv2 = SingleConv(features * 2, features)
+        self.fuse_block3 = DoubleFusion(features * 2)
+        self.fuse_conv3 = SingleConv(features * 2, features)
 
         self.final_conv = nn.Conv3d(features, out_channels, kernel_size=1)
         self.final_act = nn.Tanh()
@@ -171,21 +137,16 @@ class Unet(nn.Module):
 
         x3 = self.conv3(x2)
         x3_skip = x3.clone()
-        x3 = self.down3(x3)
+        x3 += x3_skip
 
-        x4 = self.conv4(x3)
-
-        x3_up = self.upblock3(x4)
-        x3_up = torch.cat([x3_skip, x3_up], dim=1)
-        x3_fuse = self.fuse_block3(x3_up)
-        x3_fuse = self.fuse_conv3(x3_fuse)
-
-        x2_up = self.upblock2(x3_fuse)
+        x2_up = self.upblock3(x3)
         x2_up = torch.cat([x2_skip, x2_up], dim=1)
-        x2_fuse = self.fuse_block2(x2_up)
-        x2_fuse = self.fuse_conv2(x2_fuse)
+        x2_fuse = self.fuse_block3(x2_up)
+        x2_fuse = self.fuse_conv3(x2_fuse)
 
-        x1 = self.final_conv(x2_fuse)
+        x1_up = self.upblock2(x2_fuse)
+
+        x1 = self.final_conv(x1_up)
         x1 = self.final_act(x1)
 
         return x1
