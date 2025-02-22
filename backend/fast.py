289a630f8e3bb3e705156c41eb323d3115a310f0
jas <jptjasc@gmail.com>
2025-02-22 16:29:04 -0500

    Added CORS support

--- backend/fast.py
+++ backend/fast.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
 from fastapi import File, UploadFile, FileResponse
+from fastapi.middleware.cors import CORSMiddleware
 
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app = FastAPI()
 
